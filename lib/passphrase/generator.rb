110b08ba18e65a6da6179811f00bf6caf02a33e6
Edmund Sumbar <esumbar@gmail.com>
2011-03-09 20:08:35 -0700

    More modifications toward refactoring mix method

--- lib/passphrase/generator.rb
+++ lib/passphrase/generator.rb
@@ -17,29 +17,31 @@ module Passphrase
     end
 
     def run
+      num_words = @options.num_words
       word_list = WordList.create
-      list_selector = Random.new(@num_words, 0, word_list.length - 1).to_array
-      @num_words.times do |iword|
+      list_selector = Random.new(num_words, 0, word_list.length - 1).to_array
+      num_words.times do |iword|
         word_hash = Random.new(5, 1, 6).to_array.join.to_sym
         @words << word_list[list_selector[iword]][word_hash]
       end
-      @phrase = @unmixed_phrase = @words.join(' ')
-      mix_phrase if @options.mix
+      @phrase = @unmixed_phrase = @words.join(" ")
+      mix_phrase if mix?
+    end
+    
+    def mix?
+      @options.mix
     end
 
     private
 
     def mix_phrase
+      @nchars = @phrase.length
       mixin_capital
       mixin_number
       mixin_nonalphanum
-      odd = 
-        %w( ~  &  +  :  ?  4 ) +
-        %w( !  *  [  ;  /  5 ) +
-        %w( # \(  ]  "  0  6 ) +
-        %w( $ \) \\  '  1  7 ) +
-        %w( %  -  {  <  2  8 ) +
-        %w( ^  =  }  >  3  9 )
+    end
+    
+    def xxx
       odd_char_index = Random.new(1, 0, odd.length - 1).to_array.shift
       word_index = Random.new(1, 0, @num_words - 1).to_array.shift
       word_length = @words[word_index].length
