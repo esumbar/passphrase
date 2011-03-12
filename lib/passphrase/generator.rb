d020439471ae295be1cd1352d07ec8631bf2ef52
Edmund Sumbar <esumbar@gmail.com>
2011-03-12 15:11:04 -0700

    All mix algorithms implemented

--- lib/passphrase/generator.rb
+++ lib/passphrase/generator.rb
@@ -7,6 +7,9 @@ require File.join(File.dirname(__FILE__), 'random')
 module Passphrase
   class Generator
 
+    NON_ALPHANUM_REGEX = /[~!#\$%\^&\*\(\)\-=\+\[\]\\\{\}:;"'<>\?\/]/
+    NON_ALPHANUM_ARRAY = %w( ~ ! # $ % ^ & * \( \) - = + [ ] \\ { } : ; " ' < > ? / )
+    
     attr_reader :phrase
     attr_reader :unmixed_phrase
 
@@ -37,40 +40,41 @@ module Passphrase
     def mix_phrase
       mixin_capital     unless @phrase =~ /[A-Z]/
       mixin_number      unless @phrase =~ /\d/
-      mixin_nonalphanum
+      mixin_nonalphanum unless @phrase =~ NON_ALPHANUM_REGEX
     end
     
     def mixin_capital
-      return unless @phrase =~ /[a-z]/
-      index = selected_index
+      index, character = make_character_selection
+      @phrase[index] = character.upcase
+    end
+    
+    def mixin_number
+      index, character = make_character_selection
+      numbers = ("0".."9").to_a
+      number = numbers[one_random_number(numbers.length - 1)]
+      @phrase[index] = number
+    end
+    
+    def mixin_nonalphanum
+      index, character = make_character_selection
+      len = NON_ALPHANUM_ARRAY.length
+      non_alphanum = NON_ALPHANUM_ARRAY[one_random_number(len - 1)]
+      @phrase[index] = non_alphanum
+    end
+    
+    def make_character_selection
+      return [] unless @phrase =~ /[a-z]/
+      index = one_random_number(@phrase.length - 1)
       character = @phrase.slice(index, 1)
       until character =~ /[a-z]/
         index = index.succ.modulo(@phrase.length)
         character = @phrase.slice(index, 1)
       end
-      @phrase[index] = character.upcase
+      [index, character]
     end
     
-    def mixin_number
-      numbers = ("0".."9").to_a
-      number = numbers[Random.new(1, 0, numbers.length - 1).to_array.shift]
-      @phrase[selected_index] = number
-    end
-    
-    def mixin_nonalphanum
-      non_alphanums = %w( ~ ! # $ % ^ & * \( \) - = + [ ] \\ { } : ; " ' < > ? / )
-      # TODO
-    end
-    
-    def selected_index
-      index = Random.new(1, 0, @phrase.length - 1).to_array.shift
-      word_breaks.include?(index) ? index.succ : index
-    end
-    
-    def word_breaks
-      @words[1..-2].inject([ @words.first.length ]) do |memo, word|
-        memo << (memo.last + word.length.succ)
-      end
+    def one_random_number(max)
+      Random.new(1, 0, max).to_array.shift
     end
   end
 end
